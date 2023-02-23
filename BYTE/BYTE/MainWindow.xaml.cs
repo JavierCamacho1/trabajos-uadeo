1b643be532e39b9ca97ad93afaf2ce4a639af9d4
JavierCamacho1 <ccjj9703@gmail.com>
2023-02-22 23:32:08 -0700

    trabajos 2 semestre

---
+++ BYTE/BYTE/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BYTE
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            byte primer_valor; //elementos de lenguaje
+            int segundo_valor;
+            byte tercer_valor;
+            decimal resultado;
+            if (int.Parse(textBox1.Text) < 0 || (int.Parse(textBox1.Text) > 255))
+                MessageBox.Show("Valor incorrecto (0-255)");
+            else if
+                (double.Parse(textBox2.Text) < -2147483648 || (int.Parse(textBox4.Text) > 2147483647))
+                MessageBox.Show("Valor incorrecto (0-255)");
+            else
+            {
+                primer_valor = Convert.ToByte(textBox1.Text); // byte.Parse(textBox1.Text);
+                tercer_valor = Convert.ToByte(textBox4.Text);
+                segundo_valor = Convert.ToInt32(textBox2.Text); // int.Parse(textBox2.Text);
+                resultado = primer_valor + segundo_valor + tercer_valor;
+                textBox3.Text = resultado.ToString();
+            }
+        }
+    }
+}
---
+++ mes del año/mes del año/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace mes_del_año
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int mesdelaño = 0;
+            textBox2.Text = null;
+            {
+                if (int.Parse(textBox1.Text) == 3 || (int.Parse(textBox1.Text) == 4 || (int.Parse(textBox1.Text) == 5))) textBox2.Text = ("PRIMAVERA");
+                else if
+                    (int.Parse(textBox1.Text) == 6 ||
+                    (int.Parse(textBox1.Text) == 7 ||
+                    (int.Parse(textBox1.Text) == 8))) textBox2.Text = ("VERANO");
+                else if
+                    (int.Parse(textBox1.Text) == 9 ||
+                    (int.Parse(textBox1.Text) == 10 ||
+                    (int.Parse(textBox1.Text) == 11))) textBox2.Text = ("OTOÑO");
+                else if
+                    (int.Parse(textBox1.Text) == 1 ||
+                    (int.Parse(textBox1.Text) == 2 ||
+                    (int.Parse(textBox1.Text) == 12))) textBox2.Text = ("INVIERNO");
+                else
+                {
+                    if (mesdelaño < 1 || mesdelaño > 12)
+                    {
+                        MessageBox.Show("El numero no se encuentra en el rango de los meses");
+                    }
+
+
+                }
+
+                
+
+                
+
+                    
+                   
+                   
+            }
+        }
+    }
+}
